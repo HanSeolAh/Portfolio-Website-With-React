2966fed1058cecd13f44fc15657f6ebc4725575f
HanSeolAh <92593339+HanSeolAh@users.noreply.github.com>
2022-03-16 23:05:39 +0800

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,32 @@
+export const products = [
+    {
+      id: 1,
+      img: "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=http%3A%2F%2Fcfile23.uf.tistory.com%2Fimage%2F2420DE3B52E04A4A1AE25D",
+      link: "https://hermoney.tistory.com/940",
+    },
+    {
+      id: 2,
+      img: "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=http%3A%2F%2Fcfile23.uf.tistory.com%2Fimage%2F2744173B52E04A3019967D",
+      link: "https://hermoney.tistory.com/940",
+    },
+    {
+      id: 3,
+      img: "https://i.kym-cdn.com/photos/images/newsfeed/001/295/524/cda.jpg",
+      link: "https://knowyourmeme.com/photos/1295524-longcat",
+    },
+    {
+      id: 4,
+      img: "https://i.pinimg.com/236x/f6/16/d0/f616d044c7937dde2e8ec2b4f3e6f79c--france-travel-winding-road.jpg",
+      link: "https://www.pinterest.com",
+    },
+    {
+      id: 5,
+      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQUKFttCgsOBqNNDW2o8gkLa_miUn5zNmmtg8rK5PQiXxavUE0nKBmIIeAe7h5AOGrdN8Q&usqp=CAU",
+      link: "https://www.pinterest.com",
+    },
+    {
+      id: 6,
+      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSTuh7Bf-F1X9dYSM3of76SfsJXe-Cpk9uiHWkb82a_6YuGiLrZZfXw2jdWKM7ImsRKpIM&usqp=CAU",
+      link: "https://www.pinterest.com",
+    },
+  ];
